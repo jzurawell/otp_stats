8ba32c7f7fa9c614eb7f0def6f1d369708e5882a
Jenny Zurawell <jenny@ted.com>
2014-11-19 20:09:09 -0800

    added caching to members joined

--- app/models/join.rb
+++ app/models/join.rb
@@ -10,26 +10,33 @@ class Join
        "limit" => 40
        }
 
-      response = HTTParty.get(
-        Amara::HOST + Amara::ACTIVITY_BASE_URL,
-        :query => params, :headers => Amara::HEADERS
-        ).parsed_response
+      cache_key = params.merge({
+       'after' => after.to_i/(60 * 60)
+      })
 
-      results_array = []
-      results_array.push(response['objects'])
+      Rails.cache.fetch("members_joined_#{cache_key}", expires_in: 1.hour) do
 
-      next_page = response['meta']['next']
-      until next_page.nil?
-        paginated_response = HTTParty.get(
-          Amara::HOST + next_page,
+        response = HTTParty.get(
+          Amara::HOST + Amara::ACTIVITY_BASE_URL,
           :query => params, :headers => Amara::HEADERS
           ).parsed_response
-
-        results_array.push(paginated_response['objects'])
-        next_page = paginated_response['meta']['next']
-      end
+  
+        results_array = []
+        results_array.push(response['objects'])
+  
+        next_page = response['meta']['next']
+        until next_page.nil?
+          paginated_response = HTTParty.get(
+            Amara::HOST + next_page,
+            :query => params, :headers => Amara::HEADERS
+            ).parsed_response
+  
+          results_array.push(paginated_response['objects'])
+          next_page = paginated_response['meta']['next']
+        end
 
       results_array.flatten
+      end
     end
   end
 end
